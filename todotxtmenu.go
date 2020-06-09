c261e598d824ee8e87b11ba02576870d22b22d5e
Scott Hansen <firecat4153@gmail.com>
2020-06-09 10:00:01 -0700

    Still WIP. Add item works now

--- todotxtmenu.go
+++ todotxtmenu.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os/exec"
 	"strings"
+	"time"
 
 	"github.com/JamesClonk/go-todotxt"
 )
@@ -20,14 +21,27 @@ var dmenuOpts = []string{
 }
 
 func main() {
-	var displayList strings.Builder
-	todotxt.IgnoreComments = false
-
 	tasklist, err := todotxt.LoadFromFilename("todo.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
+	edit := true
+	for edit {
+		displayList := createMenu(&tasklist)
+		out := display(displayList.String(), dmenuOpts)
+		switch {
+		case out == "Add Item":
+			addItem(&tasklist)
+		default:
+			edit = false
+			fmt.Print(tasklist)
+		}
+	}
+}
 
+func createMenu(tasklist *todotxt.TaskList) strings.Builder {
+	var displayList strings.Builder
+	displayList.WriteString("Add Item\n")
 	prior := tasklist.Filter(func(t todotxt.Task) bool {
 		return t.HasPriority()
 	})
@@ -42,17 +56,80 @@ func main() {
 		log.Fatal(err)
 	}
 	displayList.WriteString(nonprior.String())
-	out := display(displayList, dmenuOpts)
-	fmt.Println(out)
+	return displayList
 }
 
-func display(list strings.Builder, opts []string) (result string) {
+func addItem(list *todotxt.TaskList) {
+	// Add new todo item
+	task := todotxt.NewTask()
+	edit := true
+	for edit {
+		var displayList strings.Builder
+		var tdd string
+		if task.DueDate.IsZero() {
+			tdd = ""
+		} else {
+			tdd = task.DueDate.Format("2006-01-02")
+		}
+		fmt.Fprint(&displayList,
+			"Todo: "+task.Todo,
+			"\nPriority: "+task.Priority,
+			"\nProjects + (space separated): "+strings.Join(task.Projects, " "),
+			"\nContexts @ (space separated): "+strings.Join(task.Contexts, " "),
+			"\nDue date yyyy-mm-dd: "+tdd,
+			"\nThreshold yyyy-mm-dd: ",
+		)
+		out := display(displayList.String(), dmenuOpts)
+		switch {
+		case strings.HasPrefix(out, "Todo"):
+			task.Todo = display(task.Todo, append(dmenuOpts, "-p", "Todo Title: "))
+		case strings.HasPrefix(out, "Priority"):
+			task.Priority = strings.ToUpper(display(task.Priority, append(dmenuOpts, "-p", "Priority:")))
+		case strings.HasPrefix(out, "Projects"):
+			p := display(strings.Join(task.Projects, " "), append(dmenuOpts, "-p", "Projects (+):"))
+			task.Projects = strings.Split(p, " ")
+		case strings.HasPrefix(out, "Contexts"):
+			p := display(strings.Join(task.Contexts, " "), append(dmenuOpts, "-p", "Contexts (@):"))
+			task.Contexts = strings.Split(p, " ")
+		case strings.HasPrefix(out, "Due date"):
+			t := display(tdd, append(dmenuOpts, "-p", "Due Date (yyyy-mm-dd):"))
+			td, err := time.Parse("2006-01-02", t)
+			if err != nil {
+				display("", append(dmenuOpts, "-p", "Bad date format"))
+				break
+			} else {
+				task.DueDate = td
+			}
+		default:
+			edit = false
+			if task.Todo != "" {
+				list.AddTask(&task)
+			}
+		}
+	}
+}
+
+// func todoTitle(list *todotxt.TaskList) *todotxt.TaskList {
+// Prompt for new Todo item
+// 	var displayList strings.Builder
+// 	item := display(displayList, dmenuOpts)
+// 	if item != "" {
+// 		task, err := todotxt.ParseTask(item)
+// 		if err != nil {
+// 			log.Fatal(err)
+// 		}
+// 		return task
+// 	}
+// }
+
+func display(list string, opts []string) (result string) {
+	// Displays list in dmenu, returns selection
 	var out bytes.Buffer
 	var outErr bytes.Buffer
 	cmd := exec.Command(opts[0], opts[1:]...)
 	cmd.Stdout = &out
 	cmd.Stderr = &outErr
-	cmd.Stdin = strings.NewReader(list.String())
+	cmd.Stdin = strings.NewReader(list)
 	err := cmd.Run()
 	if err != nil {
 		if outErr.String() != "" {
